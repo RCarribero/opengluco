3dd2df095a3f0fe891f6fd3753d52cb37ea23c24
RCarribero <rcarribero@proton.me>
2026-08-29 16:51:28 +0200

    Release v1.1.1 - Ajustes de filtrado dinamico en informes clinicos y modales de cumplimiento

--- app-mobile/build.gradle.kts
+++ app-mobile/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.opengluco"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
         vectorDrawables {
             useSupportLibrary = true
         }
